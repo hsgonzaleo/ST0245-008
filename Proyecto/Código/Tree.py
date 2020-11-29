34a7ba4b1c8a41065013bc0e2161c3f1e258d968
Harold S. González <69641290+hsgonzaleo@users.noreply.github.com>
2020-11-29 09:47:38 -0500

    Update Tree.py

--- Proyecto/Código/Tree.py
+++ Proyecto/Código/Tree.py
@@ -214,63 +214,99 @@ def find_best_split_ID3(data, header):
 
     return best_gain, best_question
 
-
+#O(n*m*2^n)
 def build_CART_tree(data, header, limit = -1):
-
-    if limit == 0:
-        return Leaf(data)
-
-    gain, question = find_best_split_CART(data, header)
-
-    if gain == 0:
-        return Leaf(data)
-
-    true_rows, false_rows = partition(data, question)
-
-    true_child = build_CART_tree(true_rows, header, limit - 1)
-    false_child = build_CART_tree(false_rows, header, limit - 1)
-
-    return DecisionNode(question, true_child, false_child)
-
-def build_ID3_tree(data, header, limit = -1):
+    """ Recursively creates an object of type DecisionNode as a Node of a CART Tree.
     
-    if limit == 0:
+    :param data: a matrix that stores certain data such that its final column has the prediction values.
+    :param header: a list with the labels of the data to build the questions.
+    :param limit: maximum level of the tree.
+    :return: an object of type DecisionNode.
+    """
+    if limit == 0:  # If the tree arrives to the max level of recursion, creates a Leaf.
         return Leaf(data)
 
-    gain, question = find_best_split_ID3(data, header)
+    gain, question = find_best_split_CART(data, header)  # Searches the best information gain and question to divide the tree.
 
-    if gain == 0:
+    if gain == 0:  # If there is no information gain, creates a Leaf.
         return Leaf(data)
 
-    true_rows, false_rows = partition(data, question)
+    true_rows, false_rows = partition(data, question)  # Splits the data according to the best question.
 
-    true_child = build_ID3_tree(true_rows, header, limit - 1)
-    false_child = build_ID3_tree(false_rows, header, limit - 1)
+    true_child = build_CART_tree(true_rows, header, limit - 1)  # Creates the true child of the Node with the data that satisfy the question.
+    false_child = build_CART_tree(false_rows, header, limit - 1)  # Creates the false child of the Node with the data that doesn't satisfy the question.
 
     return DecisionNode(question, true_child, false_child)
 
+#O(2^n)
+def build_ID3_tree(data, header, limit = -1):
+    """ Recursively creates an object of type DecisionNode as a Node of a ID3 Tree.
+    
+    :param data: a matrix that stores certain data such that its final column has the prediction values.
+    :param header: a list with the labels of the data to build the questions.
+    :param limit: maximum level of the tree.
+    :return: an object of type DecisionNode.
+    """
+    
+    if limit == 0:  # If the tree arrives to the max level of recursion, creates a Leaf.
+        return Leaf(data)
+
+    gain, question = find_best_split_ID3(data, header)  # Searches the best information gain and question to divide the tree.
+
+    if gain == 0:  # If there is no information gain, creates a Leaf.
+        return Leaf(data)
+
+    true_rows, false_rows = partition(data, question)  # Splits the data according to the best question.
+
+    true_child = build_ID3_tree(true_rows, header, limit - 1)  # Creates the true child of the Node with the data that satisfy the question.
+    false_child = build_ID3_tree(false_rows, header, limit - 1)  # Creates the false child of the Node with the data that doesn't satisfy the question.
+
+    return DecisionNode(question, true_child, false_child)
+
+#O(2^n)
 def import_tree():
-    r = str(input("Ingrese el nombre del archivo a leer: "))
+    """ Reads a .txt file and creates an object of type DefaultTree if it is a print of a previously made CART or ID3 Tree.
+    
+    :return: tree that is the CART or ID3 tree previously made.
+    """
+    r = str(input("Ingrese el nombre del archivo a leer: "))  # Interaction with the user.
     print()
     os.system("pause")
     os.system("cls")
     print("Leyendo...\n")
-    f = open(r, "r")
-    header = f.readline()
+    
+    f = open(r, "r")  # Open the file
+    
+    header = f.readline()  # Read the labels of the data.
     header = header[2:len(header)-2].split("', '")
     header[-1] = header[-1][0:len(header[-1])-2]
-    tree = DefaultTree(read_node(f, header), header)
-    return tree
     
+    tree = DefaultTree(read_node(f, header), header)  # Creates the tree with the read_node method.
+    
+    return tree
+
+#O(2^n)
 def read_node(f, header):
+    """ With a opened file, determines with each line of the file if it refers to a Node or a Leaf and creates it.
+    
+    :param f: an opened .txt file with a saved tree.
+    :param header: the labels of the data to build the questions of the nodes.
+    :return: an object or type DecisionNode or Leaf.
+    """
     s = f.readline()
-    if s == "" or s == None:
+    
+    if s == "" or s == None: # If there is no another line, doesn't return anything.
         return None
+    
     s = s.split()
-    question = ""
+    
+    question = ""  # Checks if the string is a question of a Node and creates it.
+    
     if s[0] == "Is":
+        
         t = s[3][0:len(s[3])-1]
-        try:
+        
+        try:  # Checks if the question uses a numeric value.
             t = int(t)
             question = Question(header.index(s[1]), t, header)
         except(ValueError):
@@ -279,29 +315,50 @@ def read_node(f, header):
                 question = Question(header.index(s[1]), t, header)
             except(ValueError):
                 question = Question(header.index(s[1]), t, header)
+                
         f.readline()
-        true_child = read_node(f, header)
+        
+        true_child = read_node(f, header)  # Creates the true child with the next lines of the file.
         f.readline()
-        false_child = read_node(f, header)
+        false_child = read_node(f, header)  # Creates the false child with the next lines of the file.
+        
         return DecisionNode(question, true_child, false_child)
-    elif len(s) == 5:
+    
+    elif len(s) == 5:  # Creates the leaves.
         data = [[int(s[1][1])]]*int(s[2][0:len(s[2])-1]) + [[int(s[3][0])]]*int(s[4][0:len(s[4])-1])
+        
         return Leaf(data)
+    
     else:
         data = [[int(s[1][1])]]*int(s[2][0:len(s[2])-1])
+        
         return Leaf(data)
 
 class Question:
+    """ An object type that implements the question to decide if certain data satisfy or not a condition.
+    """
     
     #O(1)
     def __init__(self, column, value, header):
+        """ Constructs a new Question object.
+        
+        :param column: a column of certain data.
+        :param value: a value of any type.
+        :param header: the labels of the data to classify.
+        """
         self.column = column
         self.value = value
         self.header = header
     
     #O(1)
     def match(self, data):
+        """ Determines if a given data satisfies the condition of the question.
+        
+        :param data: the data to classify.
+        :return: a boolean.
+        """
         val = data[self.column]
+        
         if is_number(val) and is_number(self.value):
             return val >= self.value
         else:
@@ -309,19 +366,33 @@ class Question:
     
     #O(1)
     def __str__(self):
+        """ Prints the question with the label an value to check.
+        
+        :return: a string.
+        """
         condition = "=="
-        if is_number(self.value):
+        if is_number(self.value):  # If the question uses a numeric value.
             condition = ">="
         return "Is %s %s %s?" % (self.header[self.column], condition, str(self.value))
 
 class Leaf:
+    """ An object type that stores the possibilities that certain data get each predicition value.
+    """
     
     #O(n)
     def __init__(self, data):
+        """ Counts the cases of success and failure of a given data and storages it in a dictionary.
+        
+        :param data: a matrix that stores certain data such that its final column has the prediction values.
+        """
         self.predictions = class_counts(data)
     
     #O(1)
     def imprimir(self, spacing = ""):
+        """ Prints the cases of each prediction value appearance in the data.
+        
+        :return: a string.
+        """
         return spacing + "Prediction: " + str(self.predictions)
     
     #O(1)
@@ -351,12 +422,12 @@ class DecisionNode:
         self.true_child = true_child
         self.false_child = false_child
     
-    #O(1)
+    #O(2^n)
     def imprimir(self, spacing = ""):
         s = spacing + str(self.question) + "\n" + spacing + "--> True:" + "\n" + self.true_child.imprimir(spacing + "  ")  + "\n" + spacing + "--> False:" + "\n" + self.false_child.imprimir(spacing + "  ")
         return s
     
-    #O(1)
+    #O(2^n)
     def graph(self, digraph, name):
         digraph.node(name, str(self.question)[3:len(str(self.question))-1], style = "filled")
         digraph = self.true_child.graph(digraph, name + 't')
@@ -367,13 +438,16 @@ class DecisionNode:
 
 class CARTTree:
 
+    #O(2^n)
     def __init__(self, data, header, limit = -1):
         self.root = build_CART_tree(data, header, limit)
         self.header = header
-
+    
+    #O(2^n)
     def __str__(self):
         return str(self.header) + "\n" + self.root.imprimir()
-
+    
+    #O(2^n)
     def graphic(self):
         digraph = Digraph(format = "png")
         return self.root.graph(digraph, 'r')
@@ -381,26 +455,32 @@ class CARTTree:
 
 class ID3Tree:
     
+    #O(2^n)
     def __init__(self, data, header, limit = -1):
         self.root = build_ID3_tree(data, header, limit)
         self.header = header
-
+    
+    #O(2^n)
     def __str__(self):
         return str(self.header) + "\n" + self.root.imprimir()
-
+    
+    #O(2^n)
     def graphic(self):
         digraph = Digraph(format = "png")
         return self.root.graph(digraph, 'r')
 
 class DefaultTree:
-
+    
+    #O(1)
     def __init__(self, root, header):
         self.root = root
         self.header = header
-
+    
+    #O(2^n)
     def __str__(self):
         return str(self.header) + "\n" + self.root.imprimir()
-
+    
+    #O(2^n)
     def graphic(self):
         digraph = Digraph(format = "png")
         return self.root.graph(digraph, 'r')
