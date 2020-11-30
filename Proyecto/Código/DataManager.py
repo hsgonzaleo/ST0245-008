e68e3fcaf3f653425efe02ed69aeff40a7c77935
Harold S. González <69641290+hsgonzaleo@users.noreply.github.com>
2020-11-29 23:17:50 -0500

    Update DataManager.py

--- Proyecto/Código/DataManager.py
+++ Proyecto/Código/DataManager.py
@@ -1,34 +1,58 @@
-# Reading and managing Datasets.
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: JulianMazo, CristianZapata & HaroldGonzález
+This code uses certain data to create a CART or ID3 decision tree.
+With this tree, it will be able to make prediction of future data.
+Uses Graphviz to graph the tree.
+"""
 
-import os
-import pandas as pd
+# IMPORT LIBRARIES
 
-"Reading and storing Datasets"
+import os  # To facilitate interaction with users.
+import pandas as pd  # To read the .csv files and put the DataFrames in arrays of arrays.
+import cProfile  # To calculate the time profile of the algorithm.
 
-#O(n)
+# READ DATASETS
+
+#O(n*m)
 def read_csv():
+    """ Reads a .csv file with certain data and creates a matrix with the data.
+
+    :return: a tuple with a matrix and the labels of certain data.
+    :rtype: tuple
+    """
     os.system("cls")
-    file = str(input("\nIngrese el nombre del archivo .csv a leer: "))
-    dataset = pd.read_csv(file, sep = ';')
+    file = str(input("\nEnter the name of the .csv file to read: "))
+    dataset = pd.read_csv(file, sep = ';')  # Reads the Dataset.
+
     try:
-        filt = int(input("\nIngrese 1 si desea filtrar los datos: "))
+        filt = int(input("\nEnter 1 to filter the data, another key to continue: "))
         if filt == 1:
             dataset = filter(dataset)
     except(ValueError):
         print()
-    labels = dataset.columns.tolist()
-    data = dataset.values.tolist()
+
+    labels = dataset.columns.tolist()  # Creates the list of labels.
+    data = dataset.values.tolist()  # Creates the matrix with the data.
     print()
     os.system("pause")
     return data, labels
 
 #O(n)
 def filter(dataset):
-    switcher = {
-        1: filt_punt,
-        2: borrar_na,
+    """ Prints a menu and calls a filter function given by the user.
+
+    :param dataset: a DataFrame object.
+    :type dataset: DataFrame
+    :return: the filtered DataFrame.
+    :rtype: DataFrame
+    """
+    switcher = {  # Emulates the structure switch-cases of Java language.
+        1: filt_score,
+        2: drop_na,
         3: filt_manual,
-        4: cancelar
+        4: cancel
     }
     case = 0
     while case < 1 or case > 4:
@@ -48,19 +72,43 @@ def filter(dataset):
             print("Opción inválida. Intente de nuevo")
             print()
             os.system("pause")
-    func = switcher.get(case, lambda: cancelar)
+    func = switcher.get(case, lambda: cancel)
     dataset = func(dataset)
     return dataset
 
-def filt_punt(dataset):
+#O(1)
+def filt_score(dataset):
+    """ Filters a dataset by scores.
+
+    :param dataset: a DataFrame object.
+    :type dataset: DataFrame
+    :return: the filtered DataFrame.
+    :rtype: DataFrame
+    """
     dataset = dataset[["punt_lenguaje", "punt_matematicas", "punt_biologia", "punt_quimica", "punt_fisica", "punt_ciencias_sociales", "punt_filosofia", "punt_ingles", "exito"]]
     return dataset
 
-def borrar_na(dataset):
+#O(1)
+def drop_na(dataset):
+    """ Drops the columns of a dataset that have null values.
+
+    :param dataset: a DataFrame object.
+    :type dataset: DataFrame
+    :return: the filtered DataFrame.
+    :rtype: DataFrame
+    """
     dataset = dataset.dropna(axis = 1)
     return dataset
 
+#O(n)
 def filt_manual(dataset):
+    """ Drops columns of a dataset given by the user.
+
+    :param dataset: a DataFrame object.
+    :type dataset: DataFrame
+    :return: the filtered DataFrame.
+    :rtype: DataFrame
+    """
     col = ""
     while col != "*":
         os.system("cls")
@@ -71,12 +119,24 @@ def filt_manual(dataset):
             continue
     return dataset
 
-def cancelar(dataset):
+#O(1)
+def cancel(dataset):
+    """ Cancels the filter operation.
+
+    :param dataset: a DataFrame object.
+    :type dataset: DataFrame
+    :return: the same DataFrame.
+    :rtype: DataFrame
+    """
     return dataset
 
-
+# TESTING TIME.
 """
-# Prueba
+def testing_read_csv(file):
+    dataset = pd.read_csv(file, sep = ';')
+    labels = dataset.columns.tolist()
+    data = dataset.values.tolist()
+    return data, labels
 
-training_data, header = read_csv()
-print(header)"""
+#cProfile.run('testing_read_csv("TRAIN 4.csv")')
+"""
